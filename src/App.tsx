340c5c2e88ea889dc9a8812a5d49c9b89418428a
ndorofeeva <nat.muovi@gmail.com>
2024-07-07 12:11:00 +0200

    Add sticky footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import Header from './components/Header';
-import Router from './Router';
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Header from "./components/Header";
+import Router from "./Router";
 
-export default function App(){
+export default function App() {
   return (
-    <div className="app mx-10">
-      <Header />
-      <main>
+    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      <Container component="main" sx={{ mb: 2 }}>
+        <Header />
         <Router />
-      </main>
-      <footer>
-        <p>© Copyright 2024 Viajar&Pintar</p>
-      </footer>
-    </div>
+      </Container>
+      <Box component="footer" sx={{ px: 2, mt: "auto" }}>
+        <Container>
+          <p className="text-end">
+            © Copyright {new Date().getFullYear()} Viajar&Pintar
+          </p>
+        </Container>
+      </Box>
+    </Box>
   );
 }
